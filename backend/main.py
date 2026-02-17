803513c1cfa0440ddd57fbf496cb11652f24e3ed
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2026-02-17 14:20:59 +0100

    tested if api conection works

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,16 @@
+# backend/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Toegang verlenen aan je Next.js app (draait meestal op poort 3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
+    allow_origins=["http://localhost:3000"], # Je Next.js url
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def root():
-    return {"status": "AI Backend is online!"}
+@app.get("/api/status")
+async def get_status():
+    return {"message": "De verbinding met Python werkt!"}
