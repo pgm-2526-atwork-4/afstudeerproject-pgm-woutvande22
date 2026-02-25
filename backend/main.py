ef8a900b3a080f4019af7cb825afebf2eebb4d8e
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2026-02-25 12:52:19 +0100

    feat: integrate Supabase client and add API endpoints for status and images

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,30 @@
 # backend/main.py
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from supabase import create_client, Client
+import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], #NEXT.js URL
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 @app.get("/api/status")
 async def get_status():
     return {"message": "De verbinding met Python werkt!"}
+
+@app.get("/api/images")
+async def get_images():
+    response = supabase.table("images").select("*").execute()
+    return response.data
